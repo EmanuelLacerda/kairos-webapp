8e6ef39b656ba99b9e1bf4462f9cc0fcd4b7c74d
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-18 15:48:02 -0300

    feat(server/events/models.py): Add models

--- server/events/models.py
+++ server/events/models.py
@@ -1,3 +1,37 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class User(Base):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    # password
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Event(Base):
+    description = models.TextField(blank=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    creator = models.ForeignKey(User, related_name='events', on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User)
+    
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    def __str__(self):
+        return f"Evento criado por {self.creator.name} com a descrição '{self.description}' para o período {self.start} - {self.end}."
