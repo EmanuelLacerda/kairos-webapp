1da0860823eae07993d4e66c888b120a744de7dd
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-18 15:50:06 -0300

    feat(server/events/admin.py): Register models in the admin panel

--- server/events/admin.py
+++ server/events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Event
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = {'name', 'email', 'created_at', 'update_at', 'active'}
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = {'description', 'start', 'end', 'creator', 'participants', 'created_at', 'update_at', 'active'}
