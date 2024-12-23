5de861860f5447ede07069eca6bad708bf5a3051
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-23 15:07:27 -0300

    feat(web/src/layouts/): Add AuthLayout.vue
    After this commit, AuthLayout.vue will be the layout for login and reset password routes

---
+++ web/src/layouts/AuthLayout.vue
@@ -0,0 +1,15 @@
+<template>
+<q-layout view="hhh lpr fff">
+    <q-page-container>
+        <router-view></router-view>
+    </q-page-container>
+</q-layout>
+</template>
+
+<style lang="scss" scoped>
+    .q-layout{
+        background-image: url('../assets/auth_background.svg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
