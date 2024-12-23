5a6354bc279800b95ade83cc7d2961da38031c0d
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-23 15:08:53 -0300

    feat(web/src/router/routes.js): Add login route

--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -6,6 +6,13 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: '/auth',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: 'login', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
