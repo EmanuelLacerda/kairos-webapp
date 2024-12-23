6662336604860e4a6ef5de79ac238ae551b3696f
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-23 15:04:29 -0300

    refactor(web/src/pages/IndexPage.vue): Remove content of q-page component

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,13 +1,10 @@
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+})
+</script>
+
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
   </q-page>
 </template>
-
-<script setup>
-//
-</script>
