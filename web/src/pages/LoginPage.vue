973a3d8360ec6c275972a5939250722a13410f0c
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-23 15:08:07 -0300

    feat(web/src/pages/): Add LoginPage.vue

---
+++ web/src/pages/LoginPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'IndexPage'
+})
+
+const emailEntered = ref('')
+const passwordEntered = ref('')
+</script>
+
+<template>
+  <q-page padding>
+    <div>
+      <q-form method="post">
+        <q-input type="email" v-model="emailEntered" name="email" :autofocus="true" placeholder="E-mail">
+
+        </q-input>
+        <q-input type="password" v-model="passwordEntered" name="email" placeholder="Senha">
+
+        </q-input>
+        <q-btn label="login" type="submit">
+
+        </q-btn>
+      </q-form>
+    </div>
+  </q-page>
+</template>
