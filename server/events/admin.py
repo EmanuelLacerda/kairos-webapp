c891bf579abebe855d8181df62e40aba964121ce
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-23 07:51:47 -0300

    refactor(server/): Move User model admin from events app to new app called authentication

--- server/events/admin.py
+++ server/events/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import User, Event
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'created_at', 'update_at', 'active']
+from .models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
