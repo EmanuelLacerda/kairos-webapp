427daba3cc17d56ba39b3aebc72d1975e492b293
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-24 16:50:18 -0300

    feat(web/src/pages/): Add RegisterUserPage

---
+++ web/src/pages/RegisterUserPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineOptions({
+  name: 'RegisterUserPage'
+})
+
+import FormRegister from 'src/components/form-auth/FormRegister.vue';
+</script>
+
+<template>
+  <q-page padding class="flex items-center justify-center">
+    <section class="flex justify-center items-center">
+      <FormRegister></FormRegister>
+    </section>
+  </q-page>
+</template>
