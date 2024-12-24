18545ea2bfde0a25cd15ecb10bbbbabe261c0bae
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-24 09:10:06 -0300

    feat(web/src/layouts/MainLayout.vue): Add logout logic

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -15,6 +15,7 @@
           Quasar App
         </q-toolbar-title>
 
+        <q-btn @click="logout" v-if="authStore.isAuthenticated">Sair</q-btn>
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
@@ -48,6 +49,12 @@
 <script setup>
 import { ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
+import { useAuthStore } from 'src/stores/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const authStore = useAuthStore()
 
 const linksList = [
   {
@@ -98,5 +105,10 @@ const leftDrawerOpen = ref(false)
 
 function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+function logout(){
+  authStore.logout();
+  router.push("/auth/login/")
 }
 </script>
