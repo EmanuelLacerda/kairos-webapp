44db53a29a85cf7b01b09c6f31f7a6097a9d1a2f
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-25 20:41:12 -0300

    refactor(web/src/components/form-auth/FormLogin.vue): Remove validate password
    In the case of login process, it is not necessary the password's validation because the login process will only be successfully if both e-mail and password were valid.

--- web/src/components/form-auth/FormLogin.vue
+++ web/src/components/form-auth/FormLogin.vue
@@ -34,6 +34,8 @@ const enteredPassword = ref('')
 const errorMessageEmail = ref('')
 const errorMessagePassword = ref('')
 
+const isLoginRequestRunning = ref(false)
+
 const removeErrorMessageEmail = () => {
     errorMessageEmail.value = "";
 }
@@ -42,80 +44,41 @@ const removeErrorMessagePassword = () => {
     errorMessagePassword.value = "";
 }
 
-const validatePassword = (password) => {
-    const lowerCaseLetters = /[a-z]/g;
-    if(!password.match(lowerCaseLetters)) {
-      return {
-        "isPasswordInvalid": true,
-        "error_message": "A senha deve conter ao menos uma letra minúscula!"
-      }
-    }
-
-    const upperCaseLetters = /[A-Z]/g;
-    if(!password.match(upperCaseLetters)) {
-      return {
-        "isPasswordInvalid": true,
-        "error_message": "A senha deve conter ao menos uma letra maiúscula!"
-      }
-    }
-
-    const numbers = /[0-9]/g;
-    if(!password.match(numbers)) {
-      return {
-        "isPasswordInvalid": true,
-        "error_message": "A senha deve conter ao menos um número!"
-      }
-    }
-
-    if(!(password.length >= 8)) {
-      return {
-        "isPasswordInvalid": true,
-        "error_message": "A senha deve conter ao menos 8 caracteres!"
-      }
-    }
-
-    return {
-      "isPasswordInvalid": false
-    }
-}
-
 const submitForm = async () => {
   if(enteredEmail.value && enteredPassword.value){
-    const result = validatePassword(enteredPassword.value);
+    isLoginRequestRunning.value = true;
 
-    if(result.isPasswordInvalid){
-        errorMessagePassword.value = result.error_message;
-    } else{
-      const resultLogin = await authStore.login({
-        "email": enteredEmail.value,
-        "password": enteredPassword.value
+    const resultLogin = await authStore.login({
+      "email": enteredEmail.value,
+      "password": enteredPassword.value
+    })
+
+    if(resultLogin.wasLoginSuccessfully){
+      $q.notify({
+        message: "Login efetuado com sucesso",
+        type: "positive",
+        timeout: "500",
+        closeBtn: true
       })
 
-      if(resultLogin.wasLoginSuccessfully){
-        $q.notify({
-          message: "Login efetuado com sucesso",
-          type: "positive",
-          timeout: "500",
-          closeBtn: true
-        })
-
-        const toPath = route.query.to || '/';
-        setTimeout(() => {
-          router.push(toPath);
-        }, 600)
-      }else{
-        if(typeof resultLogin.error_message === "string"){
-            notifyWarningConfigObj.message = resultLogin.error_message;
-            $q.notify(notifyWarningConfigObj);
-        } else if(typeof resultLogin.error_message === "object"){
-            if(resultLogin.error_message.password){
-                errorMessagePassword.value = resultLogin.error_message.password[0];
-            } else if (resultLogin.error_message.email){
-                errorMessageEmail.value = resultLogin.error_message.email[0];
-            }
-        }
+      const toPath = route.query.to || '/';
+      setTimeout(() => {
+        router.push(toPath);
+      }, 600)
+    }else{
+      if(typeof resultLogin.error_message === "string"){
+          notifyWarningConfigObj.message = resultLogin.error_message;
+          $q.notify(notifyWarningConfigObj);
+      } else if(typeof resultLogin.error_message === "object"){
+          if(resultLogin.error_message.password){
+              errorMessagePassword.value = resultLogin.error_message.password[0];
+          } else if (resultLogin.error_message.email){
+              errorMessageEmail.value = resultLogin.error_message.email[0];
+          }
       }
     }
+
+    isLoginRequestRunning.value = false;
   }
 }
 </script>
@@ -125,7 +88,7 @@ const submitForm = async () => {
         <FormAuthBase @submit-form="submitForm" class="form-login">
             <InputAuthEmail v-model="enteredEmail" :autofocus="true" :error-message="errorMessageEmail" @remove-message-error="removeErrorMessageEmail"></InputAuthEmail>
             <InputAuthPassword v-model="enteredPassword" :error-message="errorMessagePassword" @remove-message-error="removeErrorMessagePassword"></InputAuthPassword>
-            <ButtonAuth button-label="Login" :is-disabled="!enteredEmail || !enteredPassword"></ButtonAuth>
+            <ButtonAuth button-label="Login" :is-disabled="!enteredEmail || !enteredPassword" :is-process-running="isLoginRequestRunning"></ButtonAuth>
         </FormAuthBase>
         <section class="container-create-account-redirect-link">
             <q-btn flat label="Não tenho conta" to="/auth/criar-conta/" />
