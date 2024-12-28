1086e1d34ba26f61876b96f5891d2c054c89feec
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-27 22:16:57 -0300

    feat(web/src/pages/IndexPage.vue): Add welcome message

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -2,9 +2,14 @@
 defineOptions({
   name: 'IndexPage'
 })
+
+import { useAuthStore } from 'src/stores/auth';
+
+const authStore = useAuthStore();
 </script>
 
 <template>
   <q-page padding>
+    <h1>Hi, {{ authStore.user.name }}!</h1>
   </q-page>
 </template>
