b6cab96769961ad76f57f31df9702db2018ec8f0
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-27 22:42:07 -0300

    feat(web/src/boot/axios.js): Add api interceptor

--- web/src/boot/axios.js
+++ web/src/boot/axios.js
@@ -1,12 +1,32 @@
 import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
+import { useAuthStore } from 'src/stores/auth'
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api/v1/' })
+const baseUrl = 'http://127.0.0.1:8000/api/v1/';
+const api = axios.create({ baseURL: baseUrl });
 
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$axios = axios
 
   app.config.globalProperties.$api = api
 })
+
+api.interceptors.request.use(async req => {
+  const authStore = useAuthStore();
+
+  const result = await authStore.checkIfUserAuthenticated();
+
+  if(result.isUserAuthenticated){
+    if(result.new_access_token){
+      authStore.updateAccessToken(result.new_access_token)
+    }
+  } else{
+    authStore.removeAuthenticatedStatus()
+  }
+
+  return req;
+})
+
+
 
 export { axios, api }
