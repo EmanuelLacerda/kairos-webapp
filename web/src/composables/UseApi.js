650cab5b934eb5391f982dfc9a6fd49e37b22eee
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-12-31 13:12:31 -0300

    feat(web/src/composables/UseApi.js/useApi): Add getUserEvents

--- web/src/composables/UseApi.js
+++ web/src/composables/UseApi.js
@@ -11,7 +11,20 @@ export default function useApi (url) {
     }       
   }
 
+  const getUserEvents = async (userId, startDate=null, endDate=null) => {
+    try {
+      if(startDate && endDate){
+        return await api.get(`auth/user/${userId}/events/?start=${startDate}&end=${endDate}`);
+      }
+
+      return await api.get(`auth/user/${userId}/events/`);      
+    } catch (error) {
+      throw new Error(error)
+    }       
+  }
+
   return {
-    postEvent
+    postEvent,
+    getUserEvents
   }
 }
