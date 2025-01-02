572c035587d1cf7353d5590e810e0b21f4b60203
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-01-02 10:04:47 -0300

    feat(web/src/services/events.js/eventsService): Add new UseApi Composable import

--- web/src/services/events.js
+++ web/src/services/events.js
@@ -1,13 +1,14 @@
 import useApi from 'src/composables/UseApi'
 
 export default function eventsService () {
-  const { getEvent, postEvent, putEvent, patchEvent, getUserEvents } = useApi('/events/')
+  const { getEvent, postEvent, putEvent, patchEvent, deleteEvent, getUserEvents } = useApi('/events/')
 
   return {
     "get": getEvent,
     "post": postEvent,
     "put": putEvent,
     "patch": patchEvent,
+    "delete": deleteEvent,
     "getUserEvents": getUserEvents
   }
 }
