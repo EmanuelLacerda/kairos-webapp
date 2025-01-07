65ba80282dfdb8b9383cb5366ff1abfb3da84a6c
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-01-07 18:10:30 -0300

    feat(web/src/pages/IndexPage.vue): Add title of page

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -8,6 +8,9 @@ import CalendarEvents from 'src/components/events/CalendarEvents.vue';
 
 <template>
   <q-page padding>
+    <section class="flex justify-center">
+      <h1 style="margin-top: 30px; font-weight: 600; color: #333;">Minha Agenda</h1>
+    </section>
     <CalendarEvents></CalendarEvents>
   </q-page>
 </template>
