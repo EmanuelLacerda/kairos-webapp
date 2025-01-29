36fe726521b9b13b799cd4c93ca2e8828dcf9501
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-01-29 17:33:39 -0300

    refactor(server/events/serializers.py/EventSerializer/validate/): Add new validations

--- server/events/serializers.py
+++ server/events/serializers.py
@@ -44,42 +44,59 @@ class EventSerializer(serializers.ModelSerializer):
             """
 
             # Situação 01: O período inicial do evento que será agendado está entre o período inicial e o período final de algum dos eventos já agendados. Para esta situação, independe qual o período final.
-            events = Event.objects.filter(
-                start__lte=start_date,
-                end__gte=start_date
+
+            currentUserEvents = Event.objects.filter(
+                creator__id=data["creator"].id
+            )
+
+            events = currentUserEvents.filter(
+                start__exact=start_date,
+                end__exact=end_date
             )
 
             if instance:
                 events = events.exclude(id__exact=instance.id)
-
+            
             if events:
-                raise serializers.ValidationError({"start_period": "O período inicial não pode estar entre o período inicial e final de outro evento! Tente novamente."})
+                raise serializers.ValidationError({"complete_period": "Já existe um evento para este exato período, a sobrescrita de eventos é inválida! Tente novamente."})
+
             else:
-                # Situação 02: Se chegou aqui, o período inicial do evento que será agendado não está entre o período inicial e o período final de algum dos eventos já agendados. Porém, o período final do mesmo pode está e o período inicial está antes do período inicial do mesmo evento agendado.
-                events = Event.objects.filter(
-                    start__lte=end_date,
-                    end__gte=end_date
+                events = currentUserEvents.filter(
+                    start__lte=start_date,
+                    end__gte=start_date
                 )
-                
 
                 if instance:
                     events = events.exclude(id__exact=instance.id)
 
                 if events:
-                    raise serializers.ValidationError({"end_period": "O período final não pode estar entre o período inicial e final de outro evento! Tente novamente."})
+                    raise serializers.ValidationError({"start_period": "O período inicial não pode estar entre o período inicial e final de outro evento! Tente novamente."})
                 else:
-                    # Situação 03: Se chegou aqui, ainda existe a possibilidade de que algum dos eventos esteja entre o período do evento que será agendado. Por exemplo, o evento que será agendado é de 10/02/2025 às 10:20 - 17/02/2025 às 17:30 e tem um evento já agendado que é 13/02/2025 às 09:20 - 15/02/2025 às 14:30.
-                    events = Event.objects.filter(
-                        start__gt=start_date,
-                        end__lt=end_date
+                    # Situação 02: Se chegou aqui, o período inicial do evento que será agendado não está entre o período inicial e o período final de algum dos eventos já agendados. Porém, o período final do mesmo pode está e o período inicial está antes do período inicial do mesmo evento agendado.
+                    events = currentUserEvents.filter(
+                        start__lte=end_date,
+                        end__gte=end_date
                     )
+                    
 
                     if instance:
                         events = events.exclude(id__exact=instance.id)
 
                     if events:
-                        raise serializers.ValidationError({"complete_period": "Um ou mais eventos iniciam e terminam entre o período inicial e final deste evento, isto é inválido! Tente novamente."})
-            
+                        raise serializers.ValidationError({"end_period": "O período final não pode estar entre o período inicial e final de outro evento! Tente novamente."})
+                    else:
+                        # Situação 03: Se chegou aqui, ainda existe a possibilidade de que algum dos eventos esteja entre o período do evento que será agendado. Por exemplo, o evento que será agendado é de 10/02/2025 às 10:20 - 17/02/2025 às 17:30 e tem um evento já agendado que é 13/02/2025 às 09:20 - 15/02/2025 às 14:30.
+                        events = currentUserEvents.filter(
+                            start__gt=start_date,
+                            end__lt=end_date
+                        )
+
+                        if instance:
+                            events = events.exclude(id__exact=instance.id)
+
+                        if events:
+                            raise serializers.ValidationError({"complete_period": "Um ou mais eventos iniciam e terminam entre o período inicial e final deste evento, isto é inválido! Tente novamente."})
+                
 
         return data
     
