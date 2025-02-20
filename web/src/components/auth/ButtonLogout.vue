43db123e5d1f0c115965e0dde096ea27522467b8
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-02-19 23:01:27 -0300

    refactor(web/src/components/auth/ButtonLogout.vue): Replace the q-btn component to the ButtonBase component
    This replacement will occur to maintain the ButtonBase usage pattern.

--- web/src/components/auth/ButtonLogout.vue
+++ web/src/components/auth/ButtonLogout.vue
@@ -6,6 +6,8 @@ defineOptions({
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
 
+import ButtonBase from '../base/ButtonBase.vue';
+
 import { useAuthStore } from 'src/stores/auth';
 import { useDialog } from 'src/composables/UseDialog';
 import { useToast } from 'src/composables/UseToast';
@@ -58,9 +60,13 @@ async function logout(){
 </script>
 
 <template>
-    <q-btn @click="logout" class="logout-btn" :loading="isLogoutRunning">
-        <i class="bi bi-box-arrow-left"></i> <span>Sair</span>
-    </q-btn>
+    <ButtonBase
+      class="logout-btn"
+      :loading="isLogoutRunning"
+      @click="logout"
+    >
+      <i class="bi bi-box-arrow-left"></i> <span>Sair</span>
+    </ButtonBase>
 </template>
 
 <style lang="scss">
