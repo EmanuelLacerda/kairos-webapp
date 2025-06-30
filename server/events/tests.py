123ea4c14d088711d9180cd87bbe2fd60a4c8454
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-06-29 22:27:45 -0300

    refactor(server/events/tests.py): Change a hardcode 401 status code to Django status code module

--- server/events/tests.py
+++ server/events/tests.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from events.models import Event
 from authentication.models import User
 from rest_framework.test import APITestCase
+from rest_framework import status
 
 class EventAPITestCase(APITestCase):
     def setUp(self):
@@ -17,13 +18,13 @@ class EventAPITestCase(APITestCase):
         url = reverse("event-list")
 
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
     def test_get_event_without_JWT_token(self):
         url = reverse("event-detail", kwargs={"pk": self.event.id})
 
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
     def test_create_event_without_JWT_token(self):
         url = reverse("event-list")
@@ -35,7 +36,7 @@ class EventAPITestCase(APITestCase):
             "end": "2025-06-30 22:38"
         }
         response = self.client.post(url, data, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
     def test_update_event_without_JWT_token(self):
         url = reverse("event-detail", kwargs={"pk": self.event.id})
@@ -44,10 +45,10 @@ class EventAPITestCase(APITestCase):
             "description": "Teste automatizado"
         }
         response = self.client.put(url, data, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
     def test_delete_event_without_JWT_token(self):
         url = reverse("event-detail", kwargs={"pk": self.event.id})
 
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
