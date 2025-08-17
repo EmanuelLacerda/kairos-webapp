5152b579185a72fc5e64b104d7f449bf9fd3c3a7
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-08-17 10:49:30 -0300

    feat(web/src/component/auth/InputAuthPassword.vue): Change the password field logic
    After this commit, it will be possible to make the password visible and invisible

--- web/src/components/auth/InputAuthPassword.vue
+++ web/src/components/auth/InputAuthPassword.vue
@@ -10,6 +10,9 @@ defineProps({
     },
 })
 
+
+import { computed, ref } from 'vue';
+
 const emit = defineEmits([
   'removeMessageError'
 ])
@@ -21,16 +24,49 @@ import InputAuthBase from './InputAuthBase.vue';
 function removeMessageError(){
     emit('removeMessageError');
 }
+
+const shouldShowPassword = ref(false)
+
+function changeThePasswordVisibility(){
+  shouldShowPassword.value = !shouldShowPassword.value
+}
+
+const passwordFieldType = computed(() => {
+  return shouldShowPassword.value ? "text" : "password"
+})
+
+const passwordFieldClasses = computed(() => {
+  return {'bi': true, 'bi-eye-fill': !shouldShowPassword.value, 'bi-eye-slash-fill': shouldShowPassword.value }
+})
 </script>
 
 <template>
     <InputAuthBase
-        type="password"
+        :type="passwordFieldType"
         name="password"
-        placeholder="Senha"
         :autofocus="autofocus"
         @remove-message-error="removeMessageError"
     >
+      <template #prepend>
         <q-icon class="bi bi-shield-lock" size="20px"></q-icon>
+      </template>
+
+      <template #append>
+        <q-icon :class="passwordFieldClasses" size="20px" id="passwordVisibleButton" @click="changeThePasswordVisibility" />
+      </template>
     </InputAuthBase>
 </template>
+
+<style lang="scss">
+form.form-auth-base{
+  label.q-field{
+    .q-field__inner{
+      .q-field__append{
+        #passwordVisibleButton:hover{
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
