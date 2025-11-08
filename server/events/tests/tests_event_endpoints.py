bf94817bade4ce5f891e9e00738f81591c5387b3
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-11-07 23:18:13 -0300

    test(server/events/tests/tests_event_endpoints.py): Add new test case class
    That new test case class will be added to gather tests related to restricting  unverified users

--- server/events/tests/tests_event_endpoints.py
+++ server/events/tests/tests_event_endpoints.py
@@ -91,14 +91,14 @@ class RestrictingAccessAndManipulationToOnlyTheAuthenticatedUserResourcesTestCas
         self.event_user_1 = Event.objects.create(
             creator=self.user_1,
             description="Realizando teste automatizado",
-            start="2025-08-12T20:30:00Z",
-            end="2025-08-12T22:30:00Z"
+            start="2026-12-12T20:30:00Z",
+            end="2026-12-12T22:30:00Z"
         )
         self.event_user_2 = Event.objects.create(
             creator=self.user_2,
             description="Realizando teste automatizado",
-            start="2025-08-14T20:30:00Z",
-            end="2025-08-14T22:30:00Z"
+            start="2026-12-14T20:30:00Z",
+            end="2026-12-14T22:30:00Z"
         )
 
         self.general_header = {'Authorization': f'Bearer {self.access_token}'}
@@ -255,3 +255,148 @@ class RestrictingAccessAndManipulationToOnlyTheAuthenticatedUserResourcesTestCas
         response = self.client.delete(url, headers=self.general_header)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+class RestrictingAccessAndManipulationToOnlyTheVerifiedUserResourcesTestCase(APITestCase):
+    def setUp(self):
+        url = reverse("auth-login")
+        self.JWT_authenticator = JWTAuthentication()
+
+
+        self.user_verified_data = {}
+
+        self.user_verified_data["instance"] = User.objects.create_user(name="user_verified", email="teste@teste123.com.br", password='test')
+        self.user_verified_data["instance"].have_email_verified = True
+        self.user_verified_data["instance"].save()
+
+        response = self.client.post(url, json.dumps({"email": "teste@teste123.com.br", "password": "test"}), content_type='application/json')
+        self.user_verified_data["access_token"] = response.data["access_token"]
+        self.user_verified_data["header"] = {'Authorization': f'Bearer {self.user_verified_data["access_token"]}'} 
+
+        self.user_verified_data["event"] = Event.objects.create(
+            creator=self.user_verified_data["instance"],
+            description="Realizando teste automatizado",
+            start="2026-12-12T20:30:00Z",
+            end="2026-12-12T22:30:00Z"
+        )
+
+
+        self.user_unverified_data = {}
+
+        self.user_unverified_data["instance"] = User.objects.create_user(name="user_unverified", email="teste@teste453.com.br", password='test')
+        self.user_unverified_data["instance"].save()
+
+        response = self.client.post(url, json.dumps({"email": "teste@teste453.com.br", "password": "test"}), content_type='application/json')
+        self.user_unverified_data["access_token"] = response.data["access_token"]
+        self.user_unverified_data["header"] = {'Authorization': f'Bearer {self.user_unverified_data["access_token"]}'} 
+
+        self.user_unverified_data["event"] = Event.objects.create(
+            creator=self.user_unverified_data["instance"],
+            description="Realizando teste automatizado",
+            start="2026-12-12T20:30:00Z",
+            end="2026-12-12T22:30:00Z"
+        )
+    
+    def test_get_all_the_user_verified_events(self):
+        all_the_user_events = Event.objects.filter(creator__id=self.user_verified_data["instance"].id)
+
+        url = reverse("event-list")
+
+        response = self.client.get(url, headers=self.user_verified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(all_the_user_events), len(response.data))
+    
+    def test_get_all_the_user_unverified_events(self):
+        url = reverse("event-list")
+
+        response = self.client.get(url, headers=self.user_unverified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_get_event_of_the_user_verified(self):
+        event_id = Event.objects.filter(creator__id=self.user_verified_data["instance"].id)[0].id
+
+        url = reverse("event-detail", kwargs={"pk": event_id})
+
+        response = self.client.get(url, headers=self.user_verified_data["header"])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_get_event_of_user_unverified(self):
+        event_id = Event.objects.filter(creator__id=self.user_unverified_data["instance"].id)[0].id
+
+        url = reverse("event-detail", kwargs={"pk": event_id})
+
+        response = self.client.get(url, headers=self.user_unverified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_event_of_the_verified_user(self):
+        data = {
+            "creator": self.user_verified_data["instance"].id,
+            "description": "Criando evento sem passar id do usuário",
+            "start": "2026-08-14T20:30:00Z",
+            "end": "2026-08-14T22:30:00Z"
+        }
+        url = reverse("event-list")
+
+        response = self.client.post(url, json.dumps(data), content_type='application/json', headers=self.user_verified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+ 
+    def test_create_event_of_the_unverified_user(self):
+        data = {
+            "creator": self.user_unverified_data["instance"].id,
+            "description": "Criando evento passando id do usuário",
+            "start": "2026-08-14T22:40:00Z",
+            "end": "2026-08-14T23:00:00Z"
+        }
+        url = reverse("event-list")
+
+        response = self.client.post(url, json.dumps(data), content_type='application/json', headers=self.user_unverified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_event_of_the_verified_user(self):
+        event = Event.objects.filter(creator__id=self.user_verified_data["instance"].id)[0]
+
+        data = {
+            "creator": event.creator.id,
+            "description": "Testando edição de eventos de usuário verificado",
+            "start": event.start.isoformat(),
+            "end": event.end.isoformat()
+        }
+        url = reverse("event-detail", kwargs={"pk": event.id})
+
+        response = self.client.patch(url, json.dumps(data), content_type='application/json', headers=self.user_verified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+ 
+    def test_update_event_of_the_unverified_user(self):
+        event = Event.objects.filter(creator__id=self.user_unverified_data["instance"].id)[0]
+
+        data = {
+            "creator": event.creator.id,
+            "description": "Testando edição de eventos de usuário verificado",
+            "start": event.start.isoformat(),
+            "end": event.end.isoformat()
+        }
+        url = reverse("event-detail", kwargs={"pk": event.id})
+
+        response = self.client.patch(url, json.dumps(data), content_type='application/json', headers=self.user_unverified_data["header"])
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_event_of_the_verified_user(self):
+        event = Event.objects.filter(creator__id=self.user_verified_data["instance"].id)[0]
+
+        url = reverse("event-detail", kwargs={"pk": event.id})
+
+        response = self.client.delete(url, headers=self.user_verified_data["header"])
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_delete_event_of_the_unverified_user(self):
+        event = Event.objects.filter(creator__id=self.user_unverified_data["instance"].id)[0]
+
+        url = reverse("event-detail", kwargs={"pk": event.id})
+
+        response = self.client.delete(url, headers=self.user_unverified_data["header"])
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
